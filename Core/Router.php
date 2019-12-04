c9b04e4b1ac645249c48c744a6a3ca44111a30f2
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-04 11:13:03 +0530

    updated code to accept custom variables of any format from the url.

--- Core/Router.php
+++ Core/Router.php
@@ -25,7 +25,20 @@
      * 
      * return void
      */
-    public function add($route, $params) {
+    public function add($route, $params = []) {
+      // convert the route to a regular expression
+      // escape forward slashes
+      $route = preg_replace('/\//', '\\/', $route);
+
+      // convert variable e.g. {controller}
+      $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+      // convert variables with custom regular expressions e.g. {id:\d+}
+      $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
+
+      // add start and end delimiters, and case insensitive flag
+      $route = '/^' . $route . '$/i';
+
       $this->routes[$route] = $params;
       // var_dump($this->routes);
     }
@@ -47,8 +60,27 @@
      * @return boolean true if a match is found, else false
      */
     public function match($url) {
-      foreach($this->routes as $route => $params) {
+      /* foreach($this->routes as $route => $params) {
         if($url == $route) {
+          $this->params = $params;
+          return true;
+        }
+      } */
+
+      // Match to the fixed url format /controller/action
+      // $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+      foreach($this->routes as $route => $params) {
+        if(preg_match($route, $url, $matches)) {
+          // get named capture group values
+          // $params = [];
+  
+          foreach($matches as $key => $match) {
+            if(is_string($key)) {
+              $params[$key] = $match;
+            }
+          }
+  
           $this->params = $params;
           return true;
         }
