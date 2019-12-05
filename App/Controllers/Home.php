df2f9815243a66d9e1efdd31753adf7c1c1bfd9b
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-05 13:53:30 +0530

    index().

---
+++ App/Controllers/Home.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+/**
+ * Home controller
+ * 
+ * PHP version 7.3.11
+ */
+
+class Home {
+  /**
+   * show the index page
+   * 
+   * @return void
+   */
+  public function index() {
+    echo "Hello from the index action in the Home controller!";
+  }
+}
+
+?>
