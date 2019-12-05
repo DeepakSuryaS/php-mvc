9bdbcf57972b6fe24d02cb31cb03d730d9b417a6
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-05 13:33:31 +0530

    index() and addNew().

---
+++ App/Controllers/Posts.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Posts controller
+ * 
+ * PHP version 7.3.11
+ */
+
+class Posts {
+  /**
+   * show the index page
+   * 
+   * @return void
+   */
+  public function index() {
+    echo "Hello from the index action in the Posts controller!";
+  }
+
+  /**
+   * show the add new page
+   * 
+   * @return void
+   */
+  public function addNew() {
+    echo "Hello from the addNew action in the Posts controller!";
+  }
+}
+
+?>
