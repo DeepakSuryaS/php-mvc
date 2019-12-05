a996fc553709c985bf108aeac0c743a56eabce18
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-05 13:55:33 +0530

    created function to dynamically require controllers.

--- public/index.php
+++ public/index.php
@@ -7,11 +7,22 @@
   // echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
 
   // require the controller class
-  require '../App/Controllers/Posts.php';
+  // require '../App/Controllers/Posts.php';
+
+  /**
+   * Autoloader
+   */
+  spl_autoload_register(function($class) {
+    $root = dirname(__DIR__); // get the parent directory
+    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
+    if(is_readable($file)) {
+      require $root . '/' . str_replace('\\', '/', $class) . '.php';
+    }
+  });
 
   // routing
-  require "../Core/Router.php";
-  $router = new Router();
+  // require "../Core/Router.php";
+  $router = new Core\Router();
   // echo get_class($router);
 
   // Add the routes
