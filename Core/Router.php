aff00edc9c65deb4b755cc6eeb201e4aaec7e89a
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-05 13:31:29 +0530

    Added dispatch() and two other methods for routing.

--- Core/Router.php
+++ Core/Router.php
@@ -28,7 +28,7 @@
     public function add($route, $params = []) {
       // convert the route to a regular expression
       // escape forward slashes
-      $route = preg_replace('/\//', '\\/', $route);
+      $route = preg_replace('/\//', '\\/', $route); // replace forward slash with a backward slash and forward slash
 
       // convert variable e.g. {controller}
       $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
@@ -95,6 +95,60 @@
      */
     public function getParams() {
       return $this->params;
+    }
+
+    /**
+     * dispatch the route, creating the controller object and running the action method
+     * 
+     * @param string $url the route url
+     * 
+     * @return void
+     */
+    public function dispatch($url) {
+      if($this->match($url)) {
+        $controller = $this->params['controller'];
+        $controller = $this->convertToStudlyCaps($controller);
+
+        if(class_exists($controller)) {
+          $controller_object = new $controller();
+          $action = $this->params['action'];
+          $action = $this->convertToCamelCase($action);
+
+          if(is_callable([$controller_object, $action])) {
+            $controller_object->$action();
+          } else {
+            echo "Method $action (in controller $controller) not found";
+          }
+        } else {
+          echo "Controller class $controller not found";
+        }
+      } else {
+        echo "No route matched.";
+      }
+    }
+
+    /**
+     * convert the string with hyphens to StudlyCaps
+     * e.g. post-authors => PostAuthors
+     * 
+     * @param string $string the string to convert
+     * 
+     * @return string
+     */
+    protected function convertToStudlyCaps($string) {
+      return str_replace(' ', '', ucwords(str_replace('-', ' ', $string)));
+    }
+
+    /**
+     * convert the string with hyphens to camelCase
+     * e.g. add-new => addNew
+     * 
+     * @param string $string the string to convert
+     * 
+     * @return string
+     */
+    protected function convertToCamelCase($string) {
+      return lcfirst($this->convertToStudlyCaps($string));
     }
   }
 ?>
