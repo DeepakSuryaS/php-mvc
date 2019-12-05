8d91c99d206f2c1f38e01bf94d8212b04501b3da
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-05 13:32:46 +0530

    require Posts controller.

--- public/index.php
+++ public/index.php
@@ -6,33 +6,39 @@
 
   // echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
 
+  // require the controller class
+  require '../App/Controllers/Posts.php';
+
+  // routing
   require "../Core/Router.php";
   $router = new Router();
   // echo get_class($router);
 
   // Add the routes
   $router->add('', ['controller' => 'Home', 'action' => 'index']);
-  $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+  //$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
   // $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
   $router->add('{controller}/{action}');
-  $router->add('admin/{action}/{controller}');
+  //$router->add('admin/{action}/{controller}');
   $router->add('{controller}/{id:\d+}/{action}');
 
   // Display the routing table
-  echo '<pre>';
+  // echo '<pre>';
   // var_dump($router->getRoutes());
-  echo htmlspecialchars(print_r($router->getRoutes(), true));
-  echo '</pre>';
+  // echo htmlspecialchars(print_r($router->getRoutes(), true));
+  // echo '</pre>';
 
   // Match the requested route
-  $url = $_SERVER['QUERY_STRING'];
+  // $url = $_SERVER['QUERY_STRING'];
 
-  if($router->match($url)) {
+  /* if($router->match($url)) {
     echo "<pre>";
     var_dump($router->getParams());
     echo "</pre>";
   } else {
     echo "No route found for URL '$url'";
-  }
+  } */
+
+  $router->dispatch($_SERVER['QUERY_STRING']);
 
 ?>
