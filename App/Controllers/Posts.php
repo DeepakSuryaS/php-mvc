017cf54fdc8bd43f70363a0f3fd682798b1c3051
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-06 14:38:05 +0530

    added 'action' suffix

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -8,13 +8,13 @@ namespace App\Controllers;
  * PHP version 7.3.11
  */
 
-class Posts {
+class Posts extends \Core\Controller {
   /**
    * show the index page
    * 
    * @return void
    */
-  public function index() {
+  public function indexAction() {
     echo "Hello from the index action in the Posts controller!";
     echo '<p>Query string parameters: <pre>' . htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
   }
@@ -24,8 +24,18 @@ class Posts {
    * 
    * @return void
    */
-  public function addNew() {
+  public function addNewAction() {
     echo "Hello from the addNew action in the Posts controller!";
+  }
+
+  /**
+   * show the edit page
+   * 
+   * @return void
+   */
+  public function editAction() {
+    echo "Hello from the edit action in the Posts controller!";
+    echo '<p>Route parameters: <pre>' . htmlspecialchars(print_r($this->route_params, true)) . '</pre></p>';
   }
 }
 
