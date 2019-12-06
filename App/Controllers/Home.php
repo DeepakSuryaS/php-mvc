aa93756490870ed48616881740ca3dfe22bc9f05
Deepak Surya S <ideepaksuryas@gmail.com>
2019-12-06 14:38:32 +0530

    added 'action' suffix and inherited before(), after().

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -8,13 +8,33 @@ namespace App\Controllers;
  * PHP version 7.3.11
  */
 
-class Home {
+class Home extends \Core\Controller {
+
+  /**
+   * before filter
+   * 
+   * @return void
+   */
+  protected function before() {
+    echo "(before) ";
+    // return false;
+  }
+
+  /**
+   * after filter
+   * 
+   * @return void
+   */
+  protected function after() {
+    echo " (after)";
+  }
+
   /**
    * show the index page
    * 
    * @return void
    */
-  public function index() {
+  public function indexAction() {
     echo "Hello from the index action in the Home controller!";
   }
 }
